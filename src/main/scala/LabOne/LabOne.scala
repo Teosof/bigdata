1808147b6b18dff654d36e8e2da5318acf34ca82
Teosof <liskunov.roma@yandex.ru>
2020-09-14 22:01:05 +0300

    [one] end of first lab

---
+++ src/main/scala/LabOne/LabOne.scala
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package LabOne
+
+import scala.util.Random
+
+object LabOne {
+  def main(args: Array[String]): Unit = {
+    val array: Seq[Int] = generator(10)
+    println(array.mkString("Input Seq(", ", ", ")"))
+    val evens: Seq[Int] = array.filter(isEven)
+    println(evens.mkString("Output Seq(", ", ", ")"))
+  }
+
+  def isEven(number: Int): Boolean = number % 2 == 0
+
+  def generator(N: Int): Seq[Int] = Seq.fill(N)(Random.nextInt(100))
+}
