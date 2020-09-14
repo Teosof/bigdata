ea2fba611f21f7182a6750095082047a2bb20cbc
Teosof <liskunov.roma@yandex.ru>
2020-09-14 23:06:22 +0300

    [two] add math of least50word

--- src/main/scala/LabTwo/LabTwo.scala
+++ src/main/scala/LabTwo/LabTwo.scala
@@ -15,18 +15,21 @@ object LabTwo {
     LogManager.getRootLogger.setLevel(WARN)
 
     val rdd: RDD[String] = sc.textFile(PATH)
-    val top50rdd: RDD[(Int, String)] = most_common_words(sc, rdd)
+
+
+    val most: RDD[(Int, String)] = common_words(sc, rdd, order = true)
+    val least: RDD[(Int, String)] = common_words(sc, rdd, order = false)
   }
 
-  private def most_common_words(sc: SparkContext, rdd: RDD[String]): RDD[(Int, String)] = {
+  private def common_words(sc: SparkContext, rdd: RDD[String], order: Boolean): RDD[(Int, String)] = {
     // word count
     val wc: RDD[(String, Int)] = rdd.flatMap(l => l.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
     // swap k,v to v,k to sort by word frequency
     val wc_swap: RDD[(Int, String)] = wc.map(_.swap)
     // sort keys by ascending=false (descending)
-    val hifreq_words: RDD[(Int, String)] = wc_swap.sortByKey(ascending = false, 1)
+    val words: RDD[(Int, String)] = wc_swap.sortByKey(ascending = order, 1)
     // get an array of top 50 frequent words
-    val top50: Array[(Int, String)] = hifreq_words.take(num = 50)
+    val top50: Array[(Int, String)] = words.take(num = 50)
     // convert array to RDD
     sc.parallelize(top50)
   }
