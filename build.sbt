856e22e90009c5ecfad046c8ee084714c3eeed1b
Teosof <liskunov.roma@yandex.ru>
2020-09-14 22:44:13 +0300

    [two] add math of top50word

--- build.sbt
+++ build.sbt
@@ -10,5 +10,8 @@ libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.7"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.7"
 
+// https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.7" % "compile"
+
 // https://mvnrepository.com/artifact/org.vegas-viz/vegas
 libraryDependencies += "org.vegas-viz" %% "vegas" % "0.3.11"
--- src/main/scala/LabTwo/LabTwo.scala
+++ src/main/scala/LabTwo/LabTwo.scala
@@ -1,6 +1,7 @@
 package LabTwo
 
-import org.apache.log4j.{Level, LogManager}
+import org.apache.log4j.Level.WARN
+import org.apache.log4j.LogManager
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -11,9 +12,22 @@ object LabTwo {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setAppName("Lab2").setMaster(s"local[$NODES]")
     val sc: SparkContext = new SparkContext(conf)
-    LogManager.getRootLogger.setLevel(Level.WARN)
+    LogManager.getRootLogger.setLevel(WARN)
 
     val rdd: RDD[String] = sc.textFile(PATH)
-    rdd.collect().foreach(println)
+    val top50rdd: RDD[(Int, String)] = most_common_words(sc, rdd)
+  }
+
+  private def most_common_words(sc: SparkContext, rdd: RDD[String]): RDD[(Int, String)] = {
+    // word count
+    val wc: RDD[(String, Int)] = rdd.flatMap(l => l.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
+    // swap k,v to v,k to sort by word frequency
+    val wc_swap: RDD[(Int, String)] = wc.map(_.swap)
+    // sort keys by ascending=false (descending)
+    val hifreq_words: RDD[(Int, String)] = wc_swap.sortByKey(ascending = false, 1)
+    // get an array of top 50 frequent words
+    val top50: Array[(Int, String)] = hifreq_words.take(num = 50)
+    // convert array to RDD
+    sc.parallelize(top50)
   }
 }
