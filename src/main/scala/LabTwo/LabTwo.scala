f0b3a53a90c52e0bcd576792f02f548ad5689ff2
Teosof <liskunov.roma@yandex.ru>
2020-09-21 23:26:05 +0300

    [two] refactoring

--- src/main/scala/LabTwo/LabTwo.scala
+++ src/main/scala/LabTwo/LabTwo.scala
@@ -2,7 +2,6 @@ package LabTwo
 
 import org.apache.log4j.Level.WARN
 import org.apache.log4j.LogManager
-import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -12,53 +11,37 @@ object LabTwo {
 
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setAppName("Lab2").setMaster(s"local[$NODES]")
-    val context: SparkContext = new SparkContext(conf)
+    val sc: SparkContext = new SparkContext(conf)
+    //    val ssc = new StreamingContext(conf, Seconds(1))
     LogManager.getRootLogger.setLevel(WARN)
 
-    val input: RDD[String] = parse(context)
-    val words: RDD[String] = context.textFile(s"$PATH/stop.csv")
-    val text: RDD[String] = stop(context, input, words)
+    val input: RDD[String] = sc.textFile(s"$PATH/var.txt")
+    //    val source: BufferedSource = Source.fromFile(s"$PATH/stop.txt")
+    //    val stop: Array[String] = try source.mkString.split("\n") finally source.close()
+    val stop: Array[String] = Array("бы", "он", "быть", "в", "весь", "вот", "все", "всей", "что", "он", "как", "но", "это", "не", "на", "его", "же", "так", "да", "вы", "она", "было", "еще")
 
-    val most: RDD[(Int, String)] = common(context, text, ascending = false)
-    val least: RDD[(Int, String)] = common(context, text, ascending = true)
+
+    val text: RDD[(String, Int)] = parse(input = input, stop = stop)
 
     println("Top50 most common words: ")
+    val most: Array[(String, Int)] = popular(text = text, ascending = false)
     most.foreach(println)
-    println("\nTop50 least common words: ")
+
+    println("Top50 least common words: ")
+    val least: Array[(String, Int)] = popular(text = text, ascending = true)
     least.foreach(println)
+
+
   }
 
-  private def parse(sc: SparkContext): RDD[String] = {
-    sc.textFile(s"$PATH/var.txt")
-      .map(_.toLowerCase())
-      .map(_.replaceAll(",", ""))
-      .map(_.replaceAll("\\.", ""))
-      .map(_.replaceAll("!", ""))
-      .map(_.replaceAll("\\?", ""))
-      .map(_.replaceAll(";", ""))
-      .map(_.replaceAll("\"", ""))
+  private def popular(text: RDD[(String, Int)], ascending: Boolean): Array[(String, Int)] = {
+    text.sortBy(_._2, ascending = ascending).take(num = 50)
   }
 
-  private def stop(sc: SparkContext, input: RDD[String], stopWordsInput: RDD[String]): RDD[String] = {
-    // Flatten, collect, and broadcast.
-    val stopWords: RDD[String] = stopWordsInput.flatMap(x => x.split("\n")).map(_.trim)
-    val broadcastStopWords: Broadcast[Set[String]] = sc.broadcast(stopWords.collect.toSet)
-
-    // Split using a regular expression that extracts words
-    val wordsWithStopWords: RDD[String] = input.flatMap(x => x.split(" "))
-    wordsWithStopWords.filter(!broadcastStopWords.value.contains(_))
-  }
-
-  private def common(sc: SparkContext, rdd: RDD[String], ascending: Boolean): RDD[(Int, String)] = {
-    // word count
-    val wc: RDD[(String, Int)] = rdd.flatMap(l => l.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
-    // swap k,v to v,k to sort by word frequency
-    val wc_swap: RDD[(Int, String)] = wc.map(_.swap)
-    // sort keys by ascending=false (descending)
-    val words: RDD[(Int, String)] = wc_swap.sortByKey(ascending = ascending, numPartitions = 1)
-    // get an array of top 50 frequent words
-    val top50: Array[(Int, String)] = words.take(num = 50)
-    // convert array to RDD
-    sc.parallelize(top50)
-  }
+  private def parse(input: RDD[String], stop: Array[String]): RDD[(String, Int)] = input
+    .flatMap(_.toLowerCase.split(" ")
+      .filter(word => word.length > 1 && !stop.contains(word)))
+    .map(_.replaceAll("[,.!?\"«» “–]", ""))
+    .map(word => (word, 1))
+    .reduceByKey(_ + _)
 }
