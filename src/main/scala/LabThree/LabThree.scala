5d1c6a58ae2606406ff2975aac1de90a062afb4c
Dmitry <cookiecatisbestgmail.com>
2020-12-06 18:27:27 +0300

    Add better plots and datafeame operations

--- src/main/scala/LabThree/LabThree.scala
+++ src/main/scala/LabThree/LabThree.scala
@@ -3,6 +3,8 @@ package LabThree
 import org.apache.log4j.Level.WARN
 import org.apache.log4j.LogManager
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions.countDistinct
+import org.apache.spark.sql.functions._
 import vegas._
 import vegas.sparkExt._
 
@@ -12,12 +14,14 @@ object LabThree {
   val NODES: Int = 3
 
   def main(args: Array[String]): Unit = {
+    //Spark initialization
     val spark: SparkSession = SparkSession.builder()
       .appName("Lab3")
       .master(s"local[$NODES]")
       .getOrCreate
     LogManager.getRootLogger.setLevel(WARN)
 
+    //Reading dataframe from file
     val df: DataFrame = spark.read
       .format("csv")
       .option("header", "true")
@@ -26,26 +30,34 @@ object LabThree {
 
     df.show(false)
 
-    val plot1 = Vegas("Job_Info").
+    //Plot of maximum Annual rate for each department
+    Vegas("Job_Info").
       withDataFrame(df).
       encodeX("Dept", Nom).
-      encodeY("Annual_Rt", Quant).
-      mark(Bar)
+      encodeY("Annual_Rt", Quant, aggregate = AggOps.Max).
+      mark(Bar).
+      show
 
-    plot1.show
-
-    val plot2 = Vegas("Job_Info").
+    //Boxplot of hourly rate for each unit
+    Vegas("Job_Info").
       withDataFrame(df).
       encodeX("Unit", Nom).
       encodeY("Hrly_Rate", Quant).
-      mark(Bar)
+      encodeColor("Unit", Nom).
+      mark(Circle).
+      show
 
-    plot2.show
+    //Plot of average hourly rate for each job
+    Vegas("Job_Info").
+      withDataFrame(df).
+      encodeX("Job_Title", Nom, sortOrder=SortOrder.Asc).
+      encodeY("Hrly_Rate", Quant, aggregate = AggOps.Mean).
+      mark(Line).
+      show
 
-    //    val df: Dataset[String] = spark.sql(s"SELECT * FROM csv.`$PATH/var.csv`").toJSON
-    //    val df: Array[Row] = spark.sql(s"SELECT * FROM csv.`$PATH/var.csv`").collect()
-    //    val df: DataFrame = spark.sql(s"SELECT * FROM csv.`$PATH/var.csv`")
-
-    
+    //Distinct number of units
+    df.agg(countDistinct("Unit")).show()
+    //Average annual rate
+    df.select(avg("Annual_Rt")).show()
   }
 }
