417be6e72d5242939bb0d0459f5e9e698bae8fa7
Teosof <liskunov.roma@yandex.ru>
2020-12-27 21:55:10 +0300

    [#4] update report and code with random forest

--- src/main/scala/LabFour/LabFour.scala
+++ src/main/scala/LabFour/LabFour.scala
@@ -2,7 +2,8 @@ package LabFour
 
 import org.apache.log4j.Level.WARN
 import org.apache.log4j.LogManager
-import org.apache.spark.ml.classification.{LogisticRegression, LogisticRegressionModel}
+import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
 import org.apache.spark.sql.functions.{col, lower}
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -26,53 +27,73 @@ object LabFour {
       .option("delimiter", ",")
       .option("inferSchema", value = true)
       .load(s"$PATH/var.csv")
+      .withColumn("Child's First Name", lower(col("Child's First Name")))
 
     dataframe.show(false)
 
-    val seq: Seq[(Int, String)] = Seq(
-      (0, "WHITE NON HISPANIC"),
-      (1, "ASIAN AND PACIFIC ISLANDER"),
-      (2, "HISPANIC"),
-      (3, "BLACK NON HISPANIC")
-    )
+    // Indexing some columns for using as features
+    val ethnicity: DataFrame = new StringIndexer()
+      .setInputCol("Ethnicity")
+      .setOutputCol("indexEthnicity")
+      .fit(dataframe)
+      .transform(dataframe)
 
-    import spark.implicits._
-    val mapper: DataFrame = seq.toDF("Ethnicity", "DistEthnicity")
-
-    val mapped: DataFrame = dataframe
-      .join(
-        mapper, dataframe("Ethnicity") === mapper("DistEthnicity"),
-        "inner"
-      )
-    val columns: Array[String] = Array("Count", "Rank")
-
+    // Vectorization of required columns
+    val cols: Array[String] = Array("indexEthnicity", "Count", "Rank")
     val assembler: VectorAssembler = new VectorAssembler()
-      .setInputCols(columns)
+      .setInputCols(cols)
       .setOutputCol("features")
-    val feature: DataFrame = assembler.transform(mapped)
+    val feature: DataFrame = assembler
+      .transform(ethnicity)
 
+    // Renaming columns for suiting SparkML
     val indexer: StringIndexer = new StringIndexer()
-      .setInputCol("DistEthnicity")
+      .setInputCol("Ethnicity")
       .setOutputCol("label")
-
     val label: DataFrame = indexer
       .fit(feature)
       .transform(feature)
     label.show(false)
 
+    // Splitting dataframe into training and test dataframes
     val seed: Int = 5043
-    val Array(training, test) = label
-      .randomSplit(Array(0.7, 0.3), seed)
+    val Array(training, test) = label.randomSplit(Array(0.7, 0.3), seed)
 
-    val regression: LogisticRegression = new LogisticRegression()
-      .setMaxIter(100)
-      .setRegParam(0.02)
-      .setElasticNetParam(0.8)
-    val model: LogisticRegressionModel = regression
+    println(s"dataframe count: ${dataframe.count()}")
+    println(s"training count: ${training.count()}")
+    println(s"test count: ${test.count()}\n")
+
+    // Train RandomForestClassifier model with training data set
+    // Setting max feature bins at 330
+    val regression: RandomForestClassifier = new RandomForestClassifier()
+      .setLabelCol("label")
+      .setFeaturesCol("features")
+      .setMaxBins(330)
+    val model: RandomForestClassificationModel = regression
       .fit(training)
 
+    // Run model with test data set to get predictions
+    // This will add new columns rawPrediction, probability and prediction
     val prediction: DataFrame = model
       .transform(test)
-    prediction.show(10)
+    prediction
+      .select("Ethnicity", "Count", "Rank", "label", "prediction")
+      .show(20)
+
+    // Select (prediction, label) and compute accuracy.
+    val evaluator: MulticlassClassificationEvaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("label")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+    val accuracy: Double = evaluator
+      .evaluate(prediction)
+
+    // Round accuracy up to 2 digits
+    println(
+      s"Accuracy = ${
+        BigDecimal(accuracy)
+          .setScale(2, BigDecimal.RoundingMode.HALF_UP)
+      }"
+    )
   }
 }
